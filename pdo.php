ff2ed28cc91bc120f5a19c2729ca42a0d811beb8
VincentBernet <54962581+VincentBernet@users.noreply.github.com>
2020-07-27 18:27:38 +0200

    first commit

---
+++ pdo.php
@@ -0,0 +1,5 @@
+<?php
+$pdo = new PDO('mysql:host=localhost;port=3306;dbname=vincent1','1234','admin');
+//To see erros see bellow
+//$pdo->setAttribute(PDO::ATTR_ERRMOD, PDO::ERROMODE_EXECPTION);
+?>
