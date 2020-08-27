0ba958f118af589c68c1981d39e3eff8058288b3
Vincent Bernet <vincent.bernet99@gmail.com>
2020-08-27 16:40:36 +0200

    master

--- header.php
+++ header.php
@@ -45,9 +45,9 @@
       
       <div class="share-button">
         <span>Check Me</span>
-          <a href="https://www.linkedin.com/in/vincent-bernet-028a64193/" ><i class="fab fa-linkedin"><img class="twitter" src="Ressource/Linkedin.png" alt="Vincent Bernet Linkedin Profile"/></i></a>
-          <a href="https://github.com/VincentBernet" ><i class="fab fa-github"><img class="twitter" src="Ressource/github.png" alt="Vincent Bernet Github Profile"/></i></a>
-          <a href="#" ><i class="fab fa-twitter"><img class="twitter" src="Ressource/Twitter.png" alt="Vincent Bernet Twitter Profile"/></i></a>
+          <a href="https://www.linkedin.com/in/vincent-bernet-028a64193/" target="_blank"><i class="fab fa-linkedin"><img class="twitter" src="Ressource/Linkedin.png" alt="Vincent Bernet Linkedin Profile"/></i></a>
+          <a href="https://github.com/VincentBernet" target="_blank"><i class="fab fa-github"><img class="twitter" src="Ressource/github.png" alt="Vincent Bernet Github Profile"/></i></a>
+          <a href="#" target="_blank"><i class="fab fa-twitter"><img class="twitter" src="Ressource/Twitter.png" alt="Vincent Bernet Twitter Profile"/></i></a>
       </div>
 
     </div>
